10b217db12582c6962a8f148b71926f3f1be8a9a
asbeel <jmfloress04@gmail.com>
2021-06-18 21:20:32 -0300

    hasta aqui todo bien, falta agregar un buen regex

--- TP6/FrmCalculadora.cs
+++ TP6/FrmCalculadora.cs
@@ -20,6 +20,7 @@ namespace TP6
         public FrmCalculadora()
         {
             InitializeComponent();
+
         }
         // este metodo lo agrego en el evento de cada numero
         private void agregarNumero(object sender, EventArgs evento)
@@ -35,18 +36,21 @@ namespace TP6
 
         private void agregarOperador(object sender, EventArgs e)
         {
-            Regex controlOperador = new Regex(@"^-?\d+(\.\d+)?$");//no filtra para lo q se creo ;-;
-            if (controlOperador.IsMatch(txtPantalla.Text))
+            Regex controlOperador = new Regex(@"^-?\d+[\.\d+]?$");//no filtra para lo q se creo ;-;
+
+            if (txtPantalla.Text != "")
             {
-                extraerDesdeAqui = txtPantalla.Text.Length + 1 ;//Guardo hasta donde esta el primer numero
+                if (controlOperador.IsMatch(txtPantalla.Text) )// proba con un or y otro regex
+                {
+                    extraerDesdeAqui = txtPantalla.Text.Length + 1;//Guardo hasta donde esta el primer numero
 
-                var btnOperador = (Button)sender;
-                calculo.Numero1 = Convert.ToSingle(txtPantalla.Text);//guardo el primer numero
-                operador = Convert.ToChar(btnOperador.Text);
-                txtPantalla.Text += btnOperador.Text;
+                    var btnOperador = (Button)sender;
+                    calculo.Numero1 = Convert.ToSingle(txtPantalla.Text);//guardo el primer numero
+                    operador = Convert.ToChar(btnOperador.Text);
+                    txtPantalla.Text += btnOperador.Text;
+                }
             }
-
-
+           
         }
 
         private void btnC_Click(object sender, EventArgs e)
@@ -68,38 +72,43 @@ namespace TP6
 
         private void btnIgual_Click(object sender, EventArgs e)
         {
-            String n2 = txtPantalla.Text;
-            n2 = n2.Substring(extraerDesdeAqui); // Esta linea Extrae desde el primer numero 
-            calculo.Numero2 = Convert.ToSingle(n2); // Luego tomamos el segundo mumero con el signo incluido  xq ToSingle se la re banca uwu
-            MessageBox.Show("El numero 1 es:" + calculo.Numero1 + "EL numero 2 es:" + calculo.Numero2);
-            switch (operador)
+            if (txtPantalla.Text != "")
             {
-                case '+':
-                    txtPantalla.Text = calculo.Suma().ToString();
-                    calculo.Numero1 = Convert.ToSingle(txtPantalla.Text);
-                    break; 
+                String n2 = txtPantalla.Text;
+                n2 = n2.Substring(extraerDesdeAqui); // Esta linea Extrae desde el primer numero 
+                calculo.Numero2 = Convert.ToSingle(n2); // Luego tomamos el segundo mumero con el signo incluido  xq ToSingle se la re banca uwu
+                switch (operador)
+                {
+                    case '+':
+                        txtPantalla.Text = calculo.Suma().ToString();
+                        calculo.Numero1 = Convert.ToSingle(txtPantalla.Text);
+                        break;
 
-                case '-':
-                    calculo.Numero1 = calculo.Resta();
-                    txtPantalla.Text = Convert.ToString(calculo.Numero1);
-                    break;
-                case '*':
-                    calculo.Numero1 = calculo.Multiplicacion();
-                    txtPantalla.Text = Convert.ToString(calculo.Numero1);
-                    break;
-
-                case '/':
-                    if(calculo.Numero2 != 0)
-                    {
-                        calculo.Numero1 = calculo.Suma();
+                    case '-':
+                        calculo.Numero1 = calculo.Resta();
                         txtPantalla.Text = Convert.ToString(calculo.Numero1);
-                    }
-                    else
-                    {
-                        MessageBox.Show("No se puede dividir por cero!!");
-                    }
-            
-                    break;
+                        break;
+                    case '*':
+                        calculo.Numero1 = calculo.Multiplicacion();
+                        txtPantalla.Text = Convert.ToString(calculo.Numero1);
+                        break;
+
+                    case '/':
+                        if (calculo.Numero2 != 0)
+                        {
+                            calculo.Numero1 = calculo.Suma();
+                            txtPantalla.Text = Convert.ToString(calculo.Numero1);
+                        }
+                        else
+                        {
+                            MessageBox.Show("No se puede dividir por cero!!");
+                        }
+
+                        break;
+                    default:
+                        MessageBox.Show("ERROR, nose que paso :c");
+                        break;
+                }
             }
         }
     }
